002f83f766a770dfabdf17f34015d37ffbbf71d6
MarijaZaiceva <3aicevamarija@gmail.com>
2026-03-10 00:51:15 +0200

    new stuff

--- label.gd
+++ label.gd
@@ -9,7 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $"../Player".get_position_delta($"../MonsterThing")<2:
-		print($"../Player".get_position_delta())
-		show()
 	pass
