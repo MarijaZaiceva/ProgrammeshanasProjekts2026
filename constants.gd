bb829d2d3a9b2f4f0289b542611f478f2535579e
MarijaZaiceva <3aicevamarija@gmail.com>
2026-03-14 01:28:40 +0200

    made a dictionary singleton file for scenes

---
+++ constants.gd
@@ -0,0 +1,8 @@
+extends Node
+#The Idea was borrowed from Queble on Youtube
+
+const GET_SCENE: Dictionary = {
+	"openGame":"uid://doel5qp1fesx8",
+	"menu":"uid://5r7nnlj7v5he",
+	"level1":"uid://dc33ark5cdfo0",
+}
