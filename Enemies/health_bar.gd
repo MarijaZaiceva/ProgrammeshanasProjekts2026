fe110e4cf1ec2cdfaf645af36d5d4243843d463b
MarijaZaiceva <3aicevamarija@gmail.com>
2026-03-18 09:43:46 +0200

    picking up items

--- Enemies/health_bar.gd
+++ Enemies/health_bar.gd
@@ -13,7 +13,6 @@ func _ready() -> void:
 	dad = self.get_parent()
 	max_health=health
 	am = health/6
-	print (dad)
 	
 	pass # Replace with function body.
 
@@ -22,7 +21,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:	
 	if player_around() and Input.is_action_just_pressed("attack"):
 		health-=dealt_damage
-		print(health)
+
 		if health >0:
 			self.text = get_health_bar()
 		else:
