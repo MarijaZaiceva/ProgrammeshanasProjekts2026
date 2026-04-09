7453b8b6e2eccfbb32b9938387a6e5e71c39db7e
MarijaZaiceva <3aicevamarija@gmail.com>
2026-04-09 13:45:37 +0300

    shaders

--- Documents/Testing_All.gd
+++ Documents/Testing_All.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 func _ready() -> void:
-	#This space is for writing tester's code.an example of a function is provided below.
+	#This space is for writing tester's code. Examples are provided below.
 	#all the functions used were copied from other code aspects.
 	#leave "pass" if nothing is written.
 	pass
