e1ca117ab4627f85fdcf77c60fc51557964ed61a
MarijaZaiceva <3aicevamarija@gmail.com>
2026-05-04 06:00:21 +0300

    -

--- MenuAndLevels/open_game.gd
+++ MenuAndLevels/open_game.gd
@@ -2,21 +2,32 @@ extends Node2D
 const menu_path = Constants.GET_SCENE.menu
 var player_data : Panel = null
 var error_area : Label = null
+var loader : CharacterBody2D = null
+
+var button_at_work:=false
 
 func _ready() -> void:
 	player_data = self.find_child("PlayerData")
 	error_area = self.find_child("Error")
+	loader = self.find_child("Loader")
 	
 
 func _on_process_input_button_down() -> void:
-	
+	if button_at_work: 
+		print("gay")
+		return
+	button_at_work=true
+	loader.visible = true
 	player_data.valid()
 	await SignalBus.got_news
+	loader.visible = false
+	
 	if player_data.valid_data:
 		SceneLoader.load_scene(menu_path)
 	else:
 		error_area.visible=true
 		error_area.text = player_data.error()
 		#set
+	button_at_work = false
 	pass # Replace with function body.
 	
--- MenuAndLevels/player_data.gd
+++ MenuAndLevels/player_data.gd
@@ -13,9 +13,15 @@ var valid_data := false
 var httpCheck:Node = null
 var httpAdd:Node = null
 
+var json
+
+
 const url = "https://handheld-emporium-irate.ngrok-free.dev/"
 
 signal server_responded
+signal check_completed
+signal addition_completed
+signal sign_in_done
 
 # For uhm when switch opertaitititi
 func _change_operation() -> void:
@@ -40,11 +46,14 @@ func _on_switch_op_button_down() -> void:
 
 #this gon be communication with the server
 func valid ():
+	valid_data=false
+	await get_tree().create_timer(0.3).timeout
+	
 	if httpCheck==null or httpAdd==null :
 		httpCheck = $CheckPlayer
 		httpAdd = $AddPlayer
-		httpCheck.request_completed.connect(self._after_checking)
-		httpAdd.request_completed.connect(self._after_adding)
+		httpCheck.request_completed.connect(self.after_checking)
+		httpAdd.request_completed.connect(self.after_adding)
 	match operationRN:
 		Operations.LOG_IN:
 			var base = get_tree().get_nodes_in_group(ButtonGroups[0])
@@ -52,21 +61,19 @@ func valid ():
 			var pas = base[1].get_text()
 			if not recieved_valid_input_([usn,pas]):
 				#result
-				valid_data=false
+				valid_data = false
 				SignalBus.got_news.emit()
 				return
 			pas=hash_that_(pas)
 			print (usn, pas)
 			log_in(usn, pas)
-			await server_responded
+			await check_completed
 			#result
-			valid_data = true
 			SignalBus.got_news.emit()
 			return
 			
 		Operations.SIGN_IN:
 			var base = get_tree().get_nodes_in_group(ButtonGroups[1])
-			print(base)
 			var usn = base[0].get_text()
 			var pas = base[1].get_text()
 			var pas2 = base[2].get_text()
@@ -82,11 +89,9 @@ func valid ():
 				SignalBus.got_news.emit()
 				return
 			pas=hash_that_(pas)
-			print (usn, pas, pas2)
-			send_n_check_(usn, pas)
-			await server_responded
+			sign_in(usn, pas)
+			await sign_in_done
 			#result
-			valid_data=true
 			SignalBus.got_news.emit()
 			return
 	#result
@@ -107,26 +112,64 @@ func recieved_valid_input_(data:Array)-> bool:
 		if regex.search(d) == null: return false
 	return true
 	
-func send_n_check_(usn:String, pas:String):
-	check_player_existance(usn, pas)
+func sign_in(usn:String, pas:String):
+	await get_tree().create_timer(0.1).timeout
+	check_player_existance(usn)
+	print("checked player existance, ")
 	await server_responded
+	if valid_data: 
+		add_user(usn, pas)
+		await addition_completed
+		sign_in_done.emit
+		print("returned valid " + str(valid_data))
+		return
+	print("returned invalid " + str(valid_data))
+	sign_in_done.emit
 	
 func log_in(usn:String, pas:String):
-	check_player_existance(usn, pas)
+	await get_tree().create_timer(0.1).timeout
+	check_player_existance(usn)
+	await server_responded
+	if valid_data: valid_data = check_password(pas, json)
+	check_completed.emit()
 	
-func check_player_existance(usn:String, pas:String):
+func check_player_existance(usn:String):
+	await get_tree().create_timer(0.1).timeout
 	var newurl = url + "players/" + usn
 	print(newurl)
 	var headers = ["Content-Type: application/json"]
 	httpCheck.request(newurl, headers, HTTPClient.METHOD_GET)
 	
-func _after_checking(results:String, response_code, headers, body):
-	var json =JSON.parse_string(body.get_string_from_utf8)
+func after_checking(results, response_code, headers, body):	
+	var json_string = body.get_string_from_utf8()
+	json = JSON.parse_string(json_string)
 	print (json)
+	match operationRN:
+		Operations.LOG_IN:
+			if json.has("username"):valid_data = true
+		Operations.SIGN_IN:
+			if json.has("detail"):valid_data = true
 	server_responded.emit()
 	
-func _after_adding(results:String, response_code, headers, body):
-	pass
+func after_adding(results, response_code, headers, body):
+	var json_string = body.get_string_from_utf8()
+	json = JSON.parse_string(json_string)
+	print (json)
+	if json.has("detail") and json.detail =="Player with that username already exists":
+		valid_data = false
+	addition_completed.emit()
+
+func check_password(pas, j)->bool:
+	return j["password_hash"]==pas
+	
+func add_user(usn, pas):
+	await get_tree().create_timer(0.1).timeout
+	var newurl = url + "players/"
+	print(newurl)
+	var headers = ["Content-Type: application/json"]
+	var body := { "username": usn, "password_hash": pas}
+	httpAdd.request(newurl, headers, HTTPClient.METHOD_POST, JSON.stringify(body))
+	
 
 
 func error () -> String:
