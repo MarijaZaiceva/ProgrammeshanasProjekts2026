a6fae5c5196569c3490fc54e616ac0cbcd2537b3
MarijaZaiceva <3aicevamarija@gmail.com>
2026-05-05 05:47:54 +0300

    -

--- Characters/player.gd
+++ Characters/player.gd
@@ -2,11 +2,20 @@ extends CharacterBody3D
 
 const SPEED = 30.0
 const JUMP_VELOCITY = 15
+var ap:Node = null
+var hb:Node = null
+
+enum Action {STILL, MOVE, JUMP}
+var last_action = Action.STILL
+var new_action
+
 
 func _ready() -> void:
 	set_physics_process(false)
 	Progress.managing_progress()
 	await SignalBus.settled_data
+	
+	ap = $MeshInstance3D/Sprite3D
 	set_physics_process(true)
 	
 	
@@ -15,9 +24,18 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.s
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 2.5
-
+	else:
+		new_action = Action.JUMP
+	
+	if is_on_floor():
+		new_action=Action.STILL
+	
+	if (velocity.x!=0 or velocity.z!=0) and is_on_floor():
+		new_action = Action.MOVE
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		new_action = Action.JUMP
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -30,7 +48,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
+	if new_action!=last_action: change_animation(new_action)
+	last_action = new_action
+	if Input.is_action_pressed("left"):
+		ap.flip_h = true
+	if Input.is_action_pressed("right"):
+		ap.flip_h = false
 	move_and_slide()
 		
 	
+func change_animation(n_a)->void:
+	match n_a:
+		Action.STILL:
+			ap.play("default")
+		Action.JUMP:
+			ap.play("jump")
+		Action.MOVE:
+			ap.play("walk")
